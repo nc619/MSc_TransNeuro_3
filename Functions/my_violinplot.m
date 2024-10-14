dbc064b333e2b323824d6570ff28b48e937fb6c5
nc619 <nc619@ic.ac.uk>
2024-10-14 16:01:20 +0100

    Added visualisation workshop

---
+++ Functions/my_violinplot.m
@@ -0,0 +1,20 @@
+function [] = my_violinplot(data, labels, show_box)
+
+arguments
+    data
+    labels = 'Data'+string(1:size(data,2))
+    show_box=true
+end
+
+violinplot([1:size(data,2)],data);
+hold on
+box_h = boxplot(data, 'Labels',labels);
+% Remove the boxes from the boxplot
+if ~show_box
+    set(findobj(gca, 'Tag', 'Box'), 'Visible', 'off');
+end
+% Customize the plot
+grid on;
+hold off
+end
+
