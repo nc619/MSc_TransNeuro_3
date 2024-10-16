4b27b87ebc626e3f445252e010a9d7238cfb0975
nc619 <nc619@ic.ac.uk>
2024-10-16 14:53:13 +0100

    Added combined workshop. Added data, and all other new workshops. Almost final version

--- Functions/my_violinplot.m
+++ Functions/my_violinplot.m
@@ -1,4 +1,4 @@
-function [] = my_violinplot(data, labels, show_box)
+function my_violinplot(data, labels, show_box)
 
 arguments
     data
@@ -8,7 +8,7 @@ end
 
 violinplot([1:size(data,2)],data);
 hold on
-box_h = boxplot(data, 'Labels',labels);
+box_h = boxplot(data, 'Labels',labels, 'PlotStyle','compact');
 % Remove the boxes from the boxplot
 if ~show_box
     set(findobj(gca, 'Tag', 'Box'), 'Visible', 'off');
