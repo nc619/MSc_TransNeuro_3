c103e2b11cacab2883b2c6038fa63fa3e88d7f47
nc619 <nc619@ic.ac.uk>
2024-10-21 16:20:14 +0100

    ex_3.1

--- Functions/exercise3_1.m
+++ Functions/exercise3_1.m
@@ -1,5 +1,3 @@
-function [output1] = exercise3_1(num1,num2,num3,num4)
-output1 = num1*num2*num3*num4;
-disp("The product is "+ output1)
+function [] = exercise3_1()
 end
 
---
+++ Functions/sol_exercise3_1.m
@@ -0,0 +1,5 @@
+function [output1] = exercise3_1(num1,num2,num3,num4)
+output1 = num1*num2*num3*num4;
+disp("The product is "+ output1)
+end
+
